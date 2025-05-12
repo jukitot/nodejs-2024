a27fbf46b3162cd84b21976324cf1f9cf28417ed
jukitot <irzhyckaya@gmail.com>
2025-05-12 08:57:44 +0300

    lesson8-hw-part2

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -29,8 +29,8 @@ const authService = {
     localStorage.setItem(_refreshToken, refreshToken)
 },
   deleteTokens():void{
-    localStorage.getItem(_accessToken)
-    localStorage.getItem(_refreshToken)
+    localStorage.removeItem(_accessToken)
+    localStorage.removeItem(_refreshToken)
   },
   me():IRes<IUser>{
     return apiService.get(urls.auth.me)
